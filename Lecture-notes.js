3ab51fb5578bc678c09eb9f352c8e9f69968ef60
NicholasHorta <nickdeveloper.js@gmail.com>
2022-12-16 16:01:19 +0000

    Lecture series 1 complete

--- Lecture-notes.js
+++ Lecture-notes.js
@@ -1,3 +1,25 @@
-//!! Lecture notes 
+//!! Documents & Collections 
 
-/// this.db.doc(<collection/docId>) - In order to access a single document
+//| In order to access a single document, this will supply us with options of how to retrieve
+/// this.db
+/// this.db.doc(<collection/docId>)
+/// .get()
+/// .subscribe((doc) => {
+///   console.log(doc.exists);
+///   console.log(doc.id);
+///   console.log(doc.data());
+/// });
+//| We can the access additional collections within the document too
+/// this.db
+/// this.db.doc(<collection/docId/collection/docId>)
+/// .get()
+/// .subscribe((doc) => {
+///   console.log(doc.exists);
+///   console.log(doc.id);
+///   console.log(doc.data());
+/// });
+
+//| Unique ID's are not generated on the server side, they are generated on the client side, 
+//| so they can be generated offline without the worry of overwriting an existing ID.
+/// 7QKOZ3JHLH40hhT2zR1b
+
--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,63 +1,106 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
+import "firebase/firestore";
 
-import 'firebase/firestore';
-
-import {AngularFirestore} from '@angular/fire/firestore';
-import {COURSES, findLessonsForCourse} from './db-data';
-
+import { AngularFirestore } from "@angular/fire/firestore";
+import { COURSES, findLessonsForCourse } from "./db-data";
+import { map, switchMap, tap } from "rxjs/operators";
+import { from } from "rxjs";
 
 @Component({
-    selector: 'about',
-    templateUrl: './about.component.html',
-    styleUrls: ['./about.component.css']
+  selector: "about",
+  templateUrl: "./about.component.html",
+  styleUrls: ["./about.component.css"],
 })
 export class AboutComponent {
+  constructor(private db: AngularFirestore) {}
 
-    constructor(private db: AngularFirestore) {
+  async uploadData() {
+    const coursesCollection = this.db.collection("courses");
+    const courses = await this.db.collection("courses").get();
+    for (let course of Object.values(COURSES)) {
+      const newCourse = this.removeId(course);
+      const courseRef = await coursesCollection.add(newCourse);
+      const lessons = await courseRef.collection("lessons");
+      const courseLessons = findLessonsForCourse(course["id"]);
+      console.log(`Uploading course ${course["description"]}`);
+      for (const lesson of courseLessons) {
+        const newLesson = this.removeId(lesson);
+        delete newLesson.courseId;
+        await lessons.add(newLesson);
+      }
     }
+  }
 
-    async uploadData() {
-        const coursesCollection = this.db.collection('courses');
-        const courses = await this.db.collection('courses').get();
-        for (let course of Object.values(COURSES)) {
-            const newCourse = this.removeId(course);
-            const courseRef = await coursesCollection.add(newCourse);
-            const lessons = await courseRef.collection('lessons');
-            const courseLessons = findLessonsForCourse(course['id']);
-            console.log(`Uploading course ${course['description']}`);
-            for (const lesson of courseLessons) {
-                const newLesson = this.removeId(lesson);
-                delete newLesson.courseId;
-                await lessons.add(newLesson);
-            }
-        }
-    }
+  removeId(data: any) {
+    const newData: any = { ...data };
+    delete newData.id;
+    return newData;
+  }
 
-    removeId(data: any) {
-        const newData: any = {...data};
-        delete newData.id;
-        return newData;
-    }
+  onReadDoc() {
+    this.db
+      .doc("courses/UCIi3v9SX6I7YhYukqWx")
+      .get()
+      .subscribe((doc) => {
+        console.log(doc.exists);
+        console.log(doc.id);
+        console.log(doc.data());
+      });
+    this.db
+      .doc("courses/UCIi3v9SX6I7YhYukqWx/lessons/7QKOZ3JHLH40hhT2zR1b")
+      .get()
+      .subscribe((doc) => {
+        console.log(doc.exists);
+        console.log(doc.id);
+        console.log(doc.data());
+      });
+  }
 
-    onReadDoc(){
-        this.db.doc
-    }
+  onReadCollection() {
+    this.db
+      .collection("courses", (ref) =>
+        ref.where("seqNo", "<=", 5).orderBy("seqNo")
+      )
+      .get()
+      .subscribe((col) => {
+        col.forEach((doc) => console.log(doc.data(), doc.id));
+      });
+    this.db
+      .collection("courses", (ref) => ref.where("seqNo", "==", 10))
+      .get()
+      .subscribe((col) => {
+        col.forEach((doc) => console.log(doc.data(), doc.id));
+      });
+    this.db
+      .collection("courses/UCIi3v9SX6I7YhYukqWx/lessons", (ref) =>
+        ref.where("seqNo", ">", 5).where("duration", "==", "4:44")
+      )
+      .get()
+      .subscribe((col) => {
+        col.forEach((doc) => console.log(doc.data(), doc.id));
+      });
+  }
 
+  onReadCollectionGroup() {
+    this.db
+      .collectionGroup("lessons", (ref) => ref.where("seqNo", "==", 1))
+      .get()
+      .subscribe((snap) => {
+        snap.forEach((i) =>
+          console.log(`%c  => `, "color: red; background: white;", i.data())
+        );
+      });
+  }
+
+  snapShotValues$;
+
+  viewSnapshotChanges() {
+    this.snapShotValues$ = this.db.doc('courses/UCIi3v9SX6I7YhYukqWx').snapshotChanges().pipe(tap(i => console.log(i)), map(i => i.payload.data()))
+  }
+
+  valueChangeValues$;
+  viewValueChanges() {
+    this.valueChangeValues$ = this.db.doc('courses/UCIi3v9SX6I7YhYukqWx').valueChanges().pipe(tap(i => console.log(i)), map(i => i))
+  }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
